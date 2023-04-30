eb61fd80c27533526da8a152e7f15cf76569b997
kkaerrung <yujeong6231@naver.com>
2023-04-30 23:54:10 +0900

    프로그래머스 Lv.1 이상한 문자 만들기:

---
+++ Lv.1/바탕화면 정리.js
@@ -0,0 +1,13 @@
+function solution(wallpaper) {
+    let [x1, y1, x2, y2] = [wallpaper.length, wallpaper[0].length, 0, 0];
+s
+    wallpaper.forEach((paper, i) => { //요소, 인덱스 
+      if (paper.includes('#')) {
+        x1 = Math.min(x1, i); 
+        y1 = Math.min(y1, paper.indexOf('#'));
+        x2 = Math.max(x2, i);
+        y2 = Math.max(y2, paper.lastIndexOf('#'));
+      }
+    });
+    return [x1, y1, x2 + 1, y2 + 1];
+  }
